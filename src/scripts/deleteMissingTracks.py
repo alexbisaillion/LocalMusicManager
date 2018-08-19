c36920d55e5d116409583453d1b5b66a6ad855a8
Alex-Bisaillion <alexbisaillion@cmail.carleton.ca>
2018-08-19 17:25:04 -0400

    Improve efficiency of deleting tracks

--- src/scripts/deleteMissingTracks.py
+++ src/scripts/deleteMissingTracks.py
@@ -1,28 +1,30 @@
-#using the iTunes COM interface for Windows to delete missing files from an iTunes library
-#install pywin32 using pip install pywin32
+# Using the iTunes COM interface for Windows to delete missing files from an iTunes library.
+# Install pywin32 using the following command:
+#   pip install pywin32
 
 import win32com.client
         
-def fetchMissingTracks():
-    iTunes = win32com.client.Dispatch("iTunes.Application")
-    library = iTunes.LibraryPlaylist
-    tracks = library.Tracks
+iTunes = win32com.client.Dispatch("iTunes.Application")
+library = iTunes.LibraryPlaylist
+tracks = library.Tracks
 
-    #iteration starts at 1, not 0
-    for x in range(1, tracks.Count+1):
-        try:
-            currTrack = tracks.Item(x)
-        except Exception as e:
-            print("Rebuilding library playlist...")
-            library = iTunes.LibraryPlaylist
-            tracks = library.Tracks
-            currTrack = tracks.Item(x)
-        try:
-            if currTrack.Kind == 1 and currTrack.location == "":
-                print("Deleting " + currTrack.name)
-                currTrack.Delete()
-        except AttributeError:
-            fetchMissingTracks()
-            break
+missingTracks = []
 
-fetchMissingTracks()
+# Iteration starts at 1, not 0.
+for x in range(1, tracks.Count+1):
+    track = win32com.client.CastTo(tracks.Item(x), 'IITFileOrCDTrack')
+    try:
+        path = track.Location
+        if path == "":
+            missingTracks.append(x)
+    except Exception as e:
+        print(e)
+    
+# When deleting a track, the next track falls into the place of the deleted track.
+# Therefore, it is necessary to keep deleting at the same index.
+for x in range(len(missingTracks)):
+    try:
+        print("Deleting " + tracks.Item(missingTracks[0]).Name + " at index " + str(missingTracks[0]) + " previously at " + str(missingTracks[x]))
+        tracks.Item(missingTracks[0]).Delete()
+    except Exception as e:
+        print(e)
