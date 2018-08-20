c95e5f85441172fe2a4146a28c8e43d420ed2f52
Alex-Bisaillion <alexbisaillion@cmail.carleton.ca>
2018-08-20 19:42:41 -0400

    Fix broken conversion status loop

--- src/scripts/convert.py
+++ src/scripts/convert.py
@@ -7,13 +7,8 @@ library = iTunes.LibraryPlaylist
 
 success = False;
 try:
-    while(not success):
-        try:
-            operationStatus = iTunes.ConvertFile2(sys.argv[1])
-            while(operationStatus.ProgressValue < operationStatus.MaxProgressValue):
-                time.sleep(1)
-            success = True;
-        except Exception as e:
-            time.sleep(1)
+    operationStatus = iTunes.ConvertFile2(sys.argv[1])
+    while(iTunes.ConvertOperationStatus is not None):
+        time.sleep(1)
 except Exception as e:
     print(e)
